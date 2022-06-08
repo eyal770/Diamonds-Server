2094866b5622171af65f0d6a8cfd292c2303f4b4
eyala <eyala@rab.gov.il>
2022-06-08 12:15:06 +0300

    push

--- DiamondsNet/DiamondsNet/Startup.cs
+++ DiamondsNet/DiamondsNet/Startup.cs
@@ -45,7 +45,6 @@ namespace DiamondsAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
